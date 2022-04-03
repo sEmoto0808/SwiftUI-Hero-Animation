5f3bfe2caffe4921e33566929b45adc174e525d6
Sho Emoto <dd2d00seg1@gmail.com>
2022-04-03 19:07:10 +0900

    ホーム画面のViewクラスを作成

---
+++ SwiftUI-Hero-Animation-Practice/Shared/View/HomePage.swift
@@ -0,0 +1,20 @@
+//
+//  HomePage.swift
+//  SwiftUI-Hero-Animation-Practice (iOS)
+//
+//  Created by Sho Emoto on 2022/04/03.
+//
+
+import SwiftUI
+
+struct HomePage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomePage_Previews: PreviewProvider {
+    static var previews: some View {
+        HomePage()
+    }
+}
