914e9b93c8debda76c16d33bd26f70476c6aeda8
Sho Emoto <dd2d00seg1@gmail.com>
2022-04-03 18:48:58 +0900

    add project

---
+++ SwiftUI-Hero-Animation-Practice/Shared/SwiftUI_Hero_Animation_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Hero_Animation_PracticeApp.swift
+//  Shared
+//
+//  Created by Sho Emoto on 2022/04/03.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Hero_Animation_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
